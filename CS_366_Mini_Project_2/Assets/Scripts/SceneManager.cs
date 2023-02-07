20eb3334fb1d425a1b21a8f58ba2b9ad47dd9c5a
Jake Buhite <jakebuhite@gmail.com>
2023-02-07 09:00:51 -0500

    Start highscore implementation

--- CS_366_Mini_Project_2/Assets/Scripts/SceneManager.cs
+++ CS_366_Mini_Project_2/Assets/Scripts/SceneManager.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class SceneManager : MonoBehaviour
 {
+    public int GameScore = 0;
+    
+    private int HighScore = 0;
+    private const string HighScoreKey = "high_score";
+    private const string RecentScoreKey = "recent_score";
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (PlayerPrefs.HasKey(HighScoreKey))
+        {
+            HighScore = PlayerPrefs.GetInt(HighScoreKey);
+        }
+        else
+        {
+            PlayerPrefs.SetInt(HighScoreKey, GameScore);
+        }
+        PlayerPrefs.SetInt(RecentScoreKey, GameScore);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void UpdateScore(int pointValue)
+    {
+        GameScore += pointValue;
+    }
+
+    public void RemoveLife()
+    {
+        // Check to see how many lives player has
+            // If player lost final life, check if high score and load game over scene
+            // Otherwise, remove life and send the player back to the origin/spawn.
     }
 }

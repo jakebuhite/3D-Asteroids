51e6a8cef5ba4dd1fe1cbcc0ee9e4f345a41e7ac
Jake Buhite <jakebuhite@gmail.com>
2023-02-09 15:58:28 -0500

    Added particle effect to player ship

---
+++ CS_366_Mini_Project_2/Assets/Scripts/Shooting.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public float cooldownTime;
+
+    private bool inCooldown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inCooldown = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && !inCooldown)
+        {
+            Shoot();
+            StartCoroutine(StartCooldown());
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit = new RaycastHit();
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 100f))
+        {
+            // Debug
+            // Debug.Log(hit.transform.name);
+            // Debug.DrawRay(this.transform.position, hit.transform.position, Color.red, 3);
+        }
+    }
+
+    IEnumerator StartCooldown()
+    {
+        inCooldown = true;
+        yield return new WaitForSeconds(cooldownTime);
+        inCooldown = false;
+    }
+
+    private void DestroyAsteroid()
+    {
+        // Check if asteroid is level 0
+            // if level 0, destroy game object
+        // If not 0, split asteroids and send them in different directions
+    }
+}

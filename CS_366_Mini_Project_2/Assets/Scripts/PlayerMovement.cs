06b131f41312e331aa4ce774b71365c76d692fda
FrozenScorpio <116769282+FrozenScorpio@users.noreply.github.com>
2023-02-09 11:36:58 -0500

    Basic Movement
    Still need to implement roll, and change values.  I am using simplicity also :D

--- CS_366_Mini_Project_2/Assets/Scripts/PlayerMovement.cs
+++ CS_366_Mini_Project_2/Assets/Scripts/PlayerMovement.cs
@@ -4,49 +4,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float forwardSpeed = 15f;
+    public float strafeSpeed = 7f;
+    public float upSpeed = 7;
 
-    public float speed;
-    public Vector3 direction;
-    public Vector3 position;
-
-    private CharacterController characterController;
+    private float aForward, aStrafe, aUp;
 
     // Start is called before the first frame update
     void Start()
     {
-        characterController = this.GetComponent<CharacterController>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        position = Camera.main.ScreenToViewportPoint(Input.mousePosition);
-        position.z = 10;
-        transform.position = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        aForward = Input.GetAxis("Vertical") * forwardSpeed;
+        aStrafe = Input.GetAxis("Mouse X") * strafeSpeed;
+        aUp = Input.GetAxis("Mouse Y") * upSpeed;
 
+        this.transform.position += transform.forward * aForward * Time.deltaTime;
+        this.transform.position += (transform.right * aStrafe * Time.deltaTime) + (transform.up * aUp * Time.deltaTime);
     }
 
-    private void Move()
-    {
-        float inputSpeed = 0;
-        float inputRotation = 0;
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-        {
-            inputSpeed = speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-            inputSpeed = -speed * Time.deltaTime;
-        }
-        Move(inputRotation, inputSpeed);
-    }
-
-    private void Move(float rot, float speed)
-    {
-        transform.Rotate(0, rot, 0);
-        // does two things: rotates forward and then translates to world space
-        Vector3 moveDirection = transform.TransformDirection(Vector3.forward) * speed;
-        characterController.Move(moveDirection);
-    }
 
 }

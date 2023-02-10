b730296ec2d3cc4d3890e6cb076353ea666de604
FrozenScorpio <116769282+FrozenScorpio@users.noreply.github.com>
2023-02-10 12:01:55 -0500

    Camera Script
    still need to add it to unity

---
+++ CS_366_Mini_Project_2/Assets/Scripts/Camera Swapping.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwapping : MonoBehaviour
+{
+
+    public GameObject thirdPerson;
+    public GameObject firstPerson;
+    private int CamMode;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            if (CamMode == 1)
+            {
+                CamMode = 0;
+            }
+            else
+            {
+                CamMode += 1;
+            }
+            StartCoroutine(camSwitching());
+        }
+    }
+
+    IEnumerator camSwitching()
+    {
+        yield return new WaitForSeconds(0.1f);
+        if (CamMode == 1)
+        {
+            thirdPerson.SetActive(true);
+            firstPerson.SetActive(false);
+        }
+        if (CamMode == 0)
+        {
+            thirdPerson.SetActive(false);
+            firstPerson.SetActive(true);
+        }
+    }
+}

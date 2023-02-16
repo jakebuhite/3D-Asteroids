c0f43c8289e99218bc10786036ffdd9d26d595ac
Jake Buhite <jakebuhite@gmail.com>
2023-02-16 08:58:04 -0500

    Finished laser ray

--- CS_366_Mini_Project_2/Assets/Scripts/Shooting.cs
+++ CS_366_Mini_Project_2/Assets/Scripts/Shooting.cs
@@ -4,54 +4,65 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
-    public float cooldownTime;
-
-    private bool inCooldown;
-
+    public float CooldownTime;
     public SceneManager Manager;
     public GameObject ShootPoint;
+
+    // Create visual effect for ray
+    public float LineWidth = 0.1f;
+    public float LineLength = 5f;
+    private LineRenderer lineRenderer;
+    private Vector3[] LinePositions = { Vector3.zero, Vector3.zero };
+
+    private bool InCooldown;
 
     // Start is called before the first frame update
     void Start()
     {
-        inCooldown = false;
+        lineRenderer = this.GetComponent<LineRenderer>();
+        lineRenderer.SetPositions(LinePositions);
+        lineRenderer.startWidth = LineWidth;
+        lineRenderer.endWidth = LineWidth;
+
+        InCooldown = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0) && !inCooldown)
+        if (Input.GetMouseButton(0) && !InCooldown)
         {
             Shoot();
+            lineRenderer.enabled = true;
             StartCoroutine(StartCooldown());
+        }
+        else
+        {
+            LinePositions[0] = Vector3.zero;
+            LinePositions[1] = Vector3.zero;
+            lineRenderer.enabled = false;
         }
     }
 
     void Shoot()
     {
         RaycastHit hit = new RaycastHit();
-        Ray ray = new(ShootPoint.transform.position, ShootPoint.transform.forward);
+        Ray ray = new(this.transform.position, -this.transform.forward);
+        Vector3 EndPoint = ray.GetPoint(100.0f);
         if (Physics.Raycast(ray, out hit, 100f))
         {
-            // Debug
-            Debug.Log(hit.transform.tag);
             Manager.SplitAsteroid(hit.transform);
             Destroy(hit.transform.gameObject);
-            // Debug.DrawRay(this.transform.position, hit.transform.position, Color.red, 3);
         }
+        LinePositions[0] = ShootPoint.transform.position;
+        LinePositions[1] = EndPoint;
+        lineRenderer.SetPositions(LinePositions);
     }
 
     IEnumerator StartCooldown()
     {
-        inCooldown = true;
-        yield return new WaitForSeconds(cooldownTime);
-        inCooldown = false;
-    }
-
-    private void DestroyAsteroid()
-    {
-        // Check if asteroid is level 0
-            // if level 0, destroy game object
-        // If not 0, split asteroids and send them in different directions
+        InCooldown = true;
+        yield return new WaitForSeconds(CooldownTime);
+        InCooldown = false;
     }
 }
